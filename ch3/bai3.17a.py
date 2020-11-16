da98d05bcf862edf1d0bac49c8a2fe5b1eadaa6a
huybv1999 <37731474+huybv1999@users.noreply.github.com>
2020-11-17 01:36:27 +0700

    sửa lại bài sai

--- ch3/bai3.17a.py
+++ ch3/bai3.17a.py
@@ -7,15 +7,23 @@ Created on Mon Oct  5 12:02:21 2020
 """ Tim prime number tu 1 cho den n"""
 
 n = int(input("nhap 1 so n : "))
-i = 2
-factors = []
-while i*i <= n:
-    if (n % i):
-        i += 1
-    else:
-        n /= i
-        factors.append(i)
-if (n > 1):
-    factors.append(int(n))
-factors = list(dict.fromkeys(factors))
-print(factors)
+# i = 2
+# factors = []
+# while i*i <= n:
+#     if (n % i):
+#         i += 1
+#     else:
+#         n /= i
+#         factors.append(i)
+# if (n > 1):
+#     factors.append(int(n))
+# factors = list(dict.fromkeys(factors))
+# print(factors)
+for i in range(1, int(n**0.5)+1):
+    if ((n % i == 0)):
+        for j in range(2,i):
+            if (i % j == 0):
+                break
+        else:
+            if (i > 1):
+                print(i)
